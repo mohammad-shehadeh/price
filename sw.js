5c2e5d2d307f2647268a26730582215589e00643
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-10 18:58:00 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'shehadeh-v3';
+const OFFLINE_URL = '/price/offline.html';
+const PRECACHE_ASSETS = [
+  '/price/',
+  '/price/index.html',
+  '/price/styles.css',
+  '/price/app.js',
+  '/price/img/icon-512x512.png',
+  '/price/img/icon-192x192.png',
+  '/price/manifest.json'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(PRECACHE_ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetching
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match(OFFLINE_URL))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => response || fetch(event.request))
+    );
+  }
+});
