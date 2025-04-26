a9c063117a2a6cc244e2d33313639a17e44958b5
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-26 13:51:57 +0300

    Create sale.js

---
+++ sale.js
@@ -0,0 +1,7 @@
+// sale.js
+const saleItems = [
+    "هاتف XYZ Pro/1624/2499/35%/https://example.com/phone.jpg",
+    "شاشة Ultra HD 32 بوصة/1349/1799/25%/https://example.com/monitor.jpg",
+    "سماعات لاسلكية/359/599/40%/https://example.com/headphones.jpg",
+    "لابتوب فائق السرعة/3599/4299/16%/https://example.com/laptop.jpg"
+];
