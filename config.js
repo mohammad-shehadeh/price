59367d28fd09ff2ce450af47ef1bb88ac8448bfc
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-05-15 21:46:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const TOKEN_PART1 = "ghp_UCTgHQsAY5MjZ9AFye5";
-const TOKEN_PART2 = "NPKty4Z4nt62H5cta";
+const TOKEN_PART1 = "ghp_‏QbaD0IOUwkEduw6";
+const TOKEN_PART2 = "‏o7oQTghtOuHYJKA11hI64";
 const GITHUB_TOKEN = TOKEN_PART1 + TOKEN_PART2;
